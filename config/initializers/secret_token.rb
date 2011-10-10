98b7ff38617700da9dcdb4f82660fd8a15a15adb
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2011-10-10 10:56:18 +0200

    absolut frisch initialisiertes rails projekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timit::Application.config.secret_token = 'ec8510deeb4147cf26c41474bdfe0ec6d9510d81b13d7598622157754b4ba523b5f6e0ea73414f0375eb89735e1beb1ebf9070e83c2b07a2bda7138eee456f34'
