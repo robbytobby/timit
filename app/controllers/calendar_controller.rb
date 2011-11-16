71022096531a7ec86f2e7f61cee6e0a81762474f
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2011-11-16 21:19:16 +0100

    Calender controllers index method now redirects to sing in path, unless authenticated

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,7 +1,6 @@
 class CalendarController < ApplicationController
-  #before_filter :authenticate_user! 
+  before_filter :authenticate_user! 
   def index
     @calendar = Calendar.new(params[:start_date], params[:end_date], params[:machines])
-    #@machines = Machine.order(:id)
   end
 end
--- spec/controllers/calendar_controller_spec.rb
+++ spec/controllers/calendar_controller_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
 describe CalendarController do
+  include Devise::TestHelpers
+
   describe "GET index" do
-    it "assigns a new calendar" do
-      params = {:start_date => Date.today.to_s, :end_date => (Date.today + 4.weeks).to_s}
-      Calendar.should_receive(:new).with(params[:start_date], params[:end_date], nil)
-      get :index, params
+    context "without user logged in" do
+      describe "GET index" do
+        it "redirects to login page" do
+          get :index
+          response.should redirect_to(new_user_session_path)
+        end
+      end
+    end
+
+    context "with user logged in" do
+      before :each do
+        sign_in FactoryGirl.create(:user)
+      end
+
+      describe "GET index" do
+        it "assigns a new calendar" do
+          params = {:start_date => Date.today.to_s, :end_date => (Date.today + 4.weeks).to_s}
+          Calendar.should_receive(:new).with(params[:start_date], params[:end_date], nil)
+          get :index, params
+        end
+      end
     end
   end
 end
