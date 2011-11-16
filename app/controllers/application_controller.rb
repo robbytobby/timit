c85c11faa12913d1ceaa1f4d662e574d9e2e37f2
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2011-11-16 22:31:08 +0100

    after sign out path geändert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+  #def after_sign_up_path_for(resource_or_scope)
+  #  new_user_session_path
+  #end
 end
