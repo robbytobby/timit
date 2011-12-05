ccfe6e6f5d04ac161f187a2f885e5cf606d3219c
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2011-12-05 16:49:43 +0100

    helper file vergessen

---
+++ spec/helpers/users_helper_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe UsersHelper do
+  describe "human_roles" do
+    #it "sets up an array for collection select with translated role and db-role" do
+    #  User.roles.each do |role|
+    #    helper.human_roles.should include([t("activerecord.attributes.user.#{role}"), role])
+    #  end
+    #end
+  end
+end
+
