4e3b126c9690ae6ea1a2544631a786348d92d8fd
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2012-01-30 13:18:11 +0100

    kommentar entfernt

--- lib/mail_interceptor.rb
+++ lib/mail_interceptor.rb
@@ -2,6 +2,5 @@ class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "TIMIT-DEV to: #{message.to}, subj: #{message.subject}"
     message.to = "christian.wittekindt@physchem.uni-freiburg.de"
-    #message.content_type = "text/html"
   end
 end
