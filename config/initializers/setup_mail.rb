fd9e7dbbad80ad1165db9b46c61864a42c593955
Widu Wittekindt <christian.wittekindt@physchem.uni-freiburg.de>
2012-01-30 10:49:48 +0100

    mailserver uni

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -14,16 +14,16 @@ if Rails.env.development?
   ActionMailer::Base.default_url_options[:host] = "localhost:3000"
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-  #:address => "physchem.uni-freiburg.de",
-  #:port => 25,
-  #:authentication => :plain,
-  #:user_name => "cw69",
-  #:password => "2qt2bstr"
-   :address => "smtp.tuxwerk.de",
-   :port => 25,
-   :authentication => :plain,
-   :user_name => "widu@radgeber-freiburg.de",
-   :password => "1ly4Mys12"
+  :address => "physchem.uni-freiburg.de",
+  :port => 25,
+  :authentication => :plain,
+  :user_name => "cw69",
+  :password => "2qt2bstr"
+  # :address => "smtp.tuxwerk.de",
+  # :port => 25,
+  # :authentication => :plain,
+  # :user_name => "widu@radgeber-freiburg.de",
+  # :password => "1ly4Mys12"
   }
   #ActionMailer::Base.raise_delivery_errors = true
 end
