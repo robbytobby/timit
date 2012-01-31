11588f3194bb87c21ba04bac6a89cfe4c5c9daa2
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2012-01-31 17:27:26 +0100

    Übersetzung eingebaut - oh, oh die specs

--- app/assets/javascripts/bookings.js.coffee
+++ app/assets/javascripts/bookings.js.coffee
@@ -4,8 +4,8 @@
 $ ->
   $('#new_booking select').live 'change', ->
     form = $(@).parents('form:first')
-    $.get form.attr('action').replace(/(\/\w+)(\/?\w*)/g,'$1/update_options'), form.serialize(), null, 'script'
+    $.get form.attr('action').replace(/(\/\D+)+((\/\d*)?\/?\w*)/g,'$1/update_options'), form.serialize(), null, 'script'
 $ ->
   $('#new_booking input').live 'change', ->
     form = $(@).parents('form:first')
-    $.get form.attr('action').replace(/(\/\w+)(\/?\w*)/g,'$1/update_options'), form.serialize(), null, 'script'
+    $.get form.attr('action').replace(/(\/\D+)+((\/\d*)?\/?\w*)/g,'$1/update_options'), form.serialize(), null, 'script'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale_from_params
   before_filter :authenticate_user! 
   
   private
+  def set_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+  
+  def default_url_options
+    {:locale => I18n.locale}
+  end
+
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
