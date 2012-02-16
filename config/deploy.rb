dbc512d4b0ee7b8e1ee0a882e4d7e18bd115ea96
Widu Wittekindt <christian.wittekindt@physchem.uni-freiburg.de>
2012-02-16 11:34:47 +0100

    deploy.rb in bezug auf rvm geändert

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,14 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, 'ruby-1.9.2-p290@timit'      
-set :rvm_type, :user 
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+#Require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+#Set :rvm_ruby_string, 'ruby-1.9.2-p290@timit'      
+#Set :rvm_type, :user 
+set :default_environment, {
+  'PATH' => "/var/www/timit2/.rvm/gems/ruby-1.9.2-p290/bin:/var/www/timit2/.rvm/bin:/var/www/timit2/.rvm/rubies/ruby-1.9.2-p290/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.9.2',
+  'GEM_HOME'     => '/var/www/timit2/.rvm/gems/ruby-1.9.2-p290@timit',
+  'GEM_PATH'     => '/var/www/timit2/.rvm/gems/ruby-1.9.2-p290@timit',
+  'BUNDLE_PATH'  => '/var/www/timit2/.rvm/gems/ruby-1.9.2-p290@timit'  # If you are using bundler.
+}
 
 require "bundler/capistrano"
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'deploy')
