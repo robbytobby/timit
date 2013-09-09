120d8898de6bfa2caa4d8b0b66f684ad95fecee6
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2013-09-09 12:11:40 +0200

    bugfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 require "bundler/capistrano"
 set :rvm_ruby_string, '1.9.2-p290@timit'      
